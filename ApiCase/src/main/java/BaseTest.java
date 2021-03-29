a0114f2d354a42d857ff2d5231074739d0efc82e
utkukilincci <utku.kilincci01@gmail.com>
2021-03-29 11:43:15 +0300

    Added BaseTest class

---
+++ ApiCase/src/main/java/BaseTest.java
@@ -0,0 +1,11 @@
+import services.CreatePlaylist;
+import services.Playlists;
+import services.Search;
+import services.User;
+
+public class BaseTest {
+    User user = new User();
+    Playlists playlists = new Playlists();
+    CreatePlaylist createPlaylist = new CreatePlaylist();
+    Search search = new Search();
+}
