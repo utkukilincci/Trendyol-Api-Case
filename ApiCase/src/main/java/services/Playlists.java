00e0e54cfef96d4dfc5957a554152185feaada4a
utkukilincci <utku.kilincci01@gmail.com>
2021-03-29 11:46:16 +0300

    fixed deleteTrack method issue

--- ApiCase/src/main/java/services/Playlists.java
+++ ApiCase/src/main/java/services/Playlists.java
@@ -1,5 +1,6 @@
 package services;
 
+import helper.Helper;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import specs.RequestSpec;
@@ -16,7 +17,7 @@ public class Playlists extends RequestSpec {
     }
 
 
-    public boolean isPlaylistNull(String playlistId,int statusCode){
+    public int getPlaylistSize(String playlistId, int statusCode){
         Response response =
                 given()
                         .spec(super.getRequestSpecification())
@@ -26,9 +27,8 @@ public class Playlists extends RequestSpec {
                         .extract()
                         .response();
         List<Object> trackList =  response.jsonPath().getList("items");
-        System.out.println(trackList.size());
-        if(trackList.size() == 0) return true;
-        return false;
+        System.out.println("Listedeki şarkı sayısı : "+trackList.size());
+        return trackList.size();
     }
 
     public void addTracks(List<String> tracks, String playlistId, int statusCode){
@@ -41,28 +41,21 @@ public class Playlists extends RequestSpec {
                     .post("/{playlist_id}/tracks",playlistId)
                     .then()
                     .spec(ResponseSpec.checkStatusCode(statusCode));
+            System.out.println(track+" : şarkısı eklendi");
         }
 
     }
 
     public void deleteTrack(String trackUri, String playlistId, int statusCode){
-        String deleteBody ="{\n" +
-                "  \"tracks\": [\n" +
-                "    {\n" +
-                "      \"uri\": " +
-                trackUri +
-                ",\n" +
-                "      \"positions\": [\n" +
-                "        2\n" +
-                "      ]\n" +
-                "    }]\n" +
-                "}";
+        JSONObject deleteBody = Helper.readJsonFile("deleteBody");
+        deleteBody.getJSONArray("tracks").getJSONObject(0).put("uri",trackUri);
 
         given()
                 .spec(super.getRequestSpecification())
-                .body(deleteBody)
+                .body(deleteBody.toString())
                 .delete("/{playlist_id}/tracks",playlistId)
                 .then()
                 .spec(ResponseSpec.checkStatusCode(statusCode));
+        System.out.println(trackUri+" : şarkısı listeden silindi");
     }
 }
